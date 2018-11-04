9d2bcc5318cd03f720178f976d44c0ef0f6bf4f1
User Macbre <maciej.brencz@gmail.com>
2018-11-04 16:26:58 +0100

    Update CSS and redirects to a main domain

--- wp-content/themes/lovecraft/footer.php
+++ wp-content/themes/lovecraft/footer.php
@@ -34,6 +34,8 @@
 
 	<div class="credits-inner section-inner">
 
+		<p>Tekst i zdjęcia: Maciej Brencz <span class="sep">&amp;</span> Materiały udostępnione na licencji CC BY 2.0</p>
+		<br>
 		<p>Napędzane przez <a href="http://www.wordpress.org">WordPress</a> <span class="sep">&amp;</span> <span>Szablon autorstwa <a href="http://www.andersnoren.se">Andersa Nor&eacute;na</a></span></p>
 
 	</div><!-- .section-inner -->
